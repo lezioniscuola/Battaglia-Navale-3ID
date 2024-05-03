1d41f21ba5ae529ba5d93f9f9988b3dafdba94fa
Localover911 <160123558+Localover911@users.noreply.github.com>
2024-05-03 13:14:24 +0200

    locatelli.java

--- locatelli.java
+++ locatelli.java
@@ -1,5 +1,19 @@
 public class locatelli {
-    public static void main(String[] args) {
-        // Crea la funzione assegnata su github
-    }
-}
+    public static int [][] colpito ( int [] [] m, int x, int y){
+        if (m[x][y] == 0){
+            System.out.println("colpo a vuoto");
+            m[x][y] = 3;
+        }
+        if (m[x][y] == 1){
+            System.out.println("colpito");
+            m[x][y] = 2;
+        }
+        if (m[x][y] == 2){
+            System.out.println("colpo su una nave già colpita");
+        }
+        if (m[x][y] == 3){
+            System.out.println("colpo a vuoto ripetuto");
+        }
+                return m;
+            }
+        }

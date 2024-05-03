9d2c312106a73853567e028326736a34f32dd144
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-05-03 13:42:10 +0200

    Update dortenzio.java

--- dortenzio.java
+++ dortenzio.java
@@ -3,7 +3,17 @@ public class dortenzio {
         private static void posizionamento(int [][] m){
         // D'Ortenzio
         //La funzione si baserà sul gestire le navi da inserire che verranno inserite dalla funzione: posizionamento() di Karaje
-        int [] navi={1,5,2,3,3,2}; //Nave piccola (1 casella):5,Veliero (2 caselle):3,Portaerei (3 caselle):2
-    }
+        int [] navi={5,3,2}; //Nave piccola (1 casella):5,Veliero (2 caselle):3,Portaerei (3 caselle):2
+        int inserite,dim=0; //Navi totali da inserire (Per comodità e compatibilità con una possibile estensione che potrà decidere le quantità di navi)
+        for (int i=1; i<=navi.length;i++){
+            inserite=0;
+            dim++;
+            while (inserite<navi[i-1]){
+                funzBattagliaNavale.posizionamento(m,dim);
+                inserite++;
+            }
+        }
+        //Terminato il ciclo sarà completato l'insermento auotmatico, grazie alla collaborazione tra le 2 funzioni.
+      }
     }
 }

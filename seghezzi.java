e9538831fafe79973ea4b593ce1e272b486c9ffd
Mikee1103 <160124042+Mikee1103@users.noreply.github.com>
2024-05-03 13:29:15 +0200

    Update seghezzi.java

--- seghezzi.java
+++ seghezzi.java
@@ -1,5 +1,12 @@
 public class seghezzi {
-    public static void main(String[] args) {
-        // Crea la funzione assegnata su github
+    public static boolean vittoria(int[][] matrice) {
+        for(int i = 0; i < matrice.length; i++) {
+            for(int j = 0; j < matrice[0].length; j++) {
+                if (matrice[i][j] == 1) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }

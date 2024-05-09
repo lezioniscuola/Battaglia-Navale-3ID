01673f9ef7580fa6982e6aca69378a95846223c3
Diego D'Ortenzio <160123629+DOrtenzio@users.noreply.github.com>
2024-05-09 11:41:58 +0200

    β
    Inserimento Karaje: Tutte le funzioni sono state inserite.

--- navi/funzBattagliaNavale.java
+++ navi/funzBattagliaNavale.java
@@ -21,8 +21,59 @@ public class funzBattagliaNavale {
         }
         //Terminato il ciclo sarà completato l'insermento auotmatico, grazie alla collaborazione tra le 2 funzioni.
     }
-    private static void posizionamento(int[][] matriceComputer, int dimensioneBarca){ //Karaje
-      //Terminare
+    public static void posizionamento(int[][] matriceComputer, int dimensioneBarca) { //Karaje
+        int righe = matriceComputer.length;
+        int colonne = matriceComputer[0].length;
+        int x = (int) (Math.random() * righe);
+        int y = (int) (Math.random() * colonne);
+        int direzione = (int) (Math.random() * 2);
+        boolean posizionato = false;
+        if (dimensioneBarca >= 4 || dimensioneBarca < 1) {
+            throw new IllegalArgumentException("Dimensione barca non valida"); // In caso in cui la dimensione della barca non è valida si lancia un errore
+        }
+        if (dimensioneBarca > righe || dimensioneBarca > colonne) {
+            throw new IllegalArgumentException("La barca non può stare nel campo di battaglia"); // In caso in cui la barca non può stare nel campo si lancia un errore
+        }
+        while (!posizionato) {
+            if (direzione == 0) {
+                if (x + dimensioneBarca < righe) {
+                    for (int i = 0; i < dimensioneBarca; i++) {
+                        if (matriceComputer[x + i][y] == 0) {
+                            posizionato = true;
+                        } else {
+                            posizionato = false;
+                            break;
+                        }
+                    }
+                    if (posizionato) {
+                        for (int i = 0; i < dimensioneBarca; i++) {
+                            matriceComputer[x + i][y] = dimensioneBarca;
+                        }
+                    }
+                }
+            } else {
+                if (y + dimensioneBarca < colonne) {
+                    for (int i = 0; i < dimensioneBarca; i++) {
+                        if (matriceComputer[x][y + i] == 0) {
+                            posizionato = true;
+                        } else {
+                            posizionato = false;
+                            break;
+                        }
+                    }
+                    if (posizionato) {
+                        for (int i = 0; i < dimensioneBarca; i++) {
+                            matriceComputer[x][y + i] = dimensioneBarca;
+                        }
+                    }
+                }
+            }
+            if (!posizionato) {
+                x = (int) (Math.random() * 10);
+                y = (int) (Math.random() * 10);
+                direzione = (int) (Math.random() * 2);
+            }
+        }
     }
    public static String stampaCampoGiocatore(int [][] matrice){ //Viapiana
         String s="    "; //Stringa inizializzata con lo spazio che occuperà la colonna verticale di indici e separatori

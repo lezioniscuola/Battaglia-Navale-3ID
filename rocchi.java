a48fe446a1a7b8835f980f32b1a0e7584a80035f
Sebastiano <160122164+Sebahhhhh@users.noreply.github.com>
2024-05-09 11:34:33 +0200

    Done rocchi.java
    Work Added

--- rocchi.java
+++ rocchi.java
@@ -1,5 +1,24 @@
-public class rocchi {
-    public static void main(String[] args) {
-        // Crea la funzione assegnata su github
-    }
+public static void colpoPng (int [][] m, int livello) { 
+    //matrice = colpi livello = numeri di colpi inseriti
+    
+    int riga,colonna;
+    boolean isGiaPresente=false;
+    Random rand = new Random();
+    do {
+        // Generazione di due numeri casuali compresi tra 0 e n
+        riga = rand.nextInt(m[0][0]); //m[0][0]=grandezza campo
+        colonna = rand.nextInt(m[0][0]);
+        for (int i=1;i<=livello;i++){
+            //Controllo matrice colpi
+            if (m[i][0] == riga && m[i][1] == colonna){
+                isGiaPresente=true;
+                break;
+            }
+        }
+    }while (isGiaPresente);
+    m[livello][0]=riga;
+    m[livello][1]=colonna;
 }
+
+
+//funziona un bordello
